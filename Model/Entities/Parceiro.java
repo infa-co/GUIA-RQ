a62b36ec0bc545ca8a5a86dee5666270b801a516
maZero1 <tkmarlon594@gmail.com>
2025-10-14 20:15:35 -0300

    Refactor Parceiro class setters to include validation checks for input values

--- Model/Entities/Parceiro.java
+++ Model/Entities/Parceiro.java
@@ -18,31 +18,48 @@ public class Parceiro {
         return nomeFantasia;
     }
     public void setNomeFantasia(String nomeFantasia) {
-        this.nomeFantasia = nomeFantasia;
+        if (!nomeFantasia.isEmpty() && nomeFantasia != null) {
+            this.nomeFantasia = nomeFantasia;
+        }
     }
     public String getCnpj() {
         return cnpj;
     }
     public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
+        if(cnpj.length() == 14){
+            this.cnpj = cnpj;
+        } else {
+            throw new IllegalArgumentException("CNPJ deve ter 14 caracteres.");
+        }
     }
     public String getEndereco() {
         return endereco;
     }
     public void setEndereco(String endereco) {
-        this.endereco = endereco;
+        if (!endereco.isEmpty() && endereco != null) {
+            this.endereco = endereco; 
+        }else {
+            throw new IllegalArgumentException("Endereço não pode ser vazio.");
     }
     public String getDescricao() {
         return descricao;
     }
     public void setDescricao(String descricao) {
-        this.descricao = descricao;
+        if (!descricao.isEmpty() && descricao != null) {
+            this.descricao = descricao;
+        } else {
+            throw new IllegalArgumentException("Descrição não pode ser vazia.");
+        }
     }
     public String getTelefone() {
         return telefone;
     }
     public void setTelefone(String telefone) {
-        this.telefone = telefone;
+        if (telefone.length() >= 10 && telefone.length() <= 11) {
+            this.telefone = telefone;
+        } else {
+            throw new IllegalArgumentException("Telefone deve ter entre 10 e 11 caracteres.");
+        }
     }
     public List<Ticket> getTicketsOferecidos() {
         for ( Ticket ticket : ticketsOferecidos) {
