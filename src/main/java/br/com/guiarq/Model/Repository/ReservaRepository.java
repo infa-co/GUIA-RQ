6683a9101afd3b1c8399042a81db7a01b03c8115
maZero1 <tkmarlon594@gmail.com>
2025-11-08 11:41:46 -0300

    Infraestrutura do back-end feita

---
+++ src/main/java/br/com/guiarq/Model/Repository/ReservaRepository.java
@@ -0,0 +1,9 @@
+package br.com.guiarq.Model.Repository;
+
+import br.com.guiarq.Model.Entities.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReservaRepository {
+}
