e55dbc7e520835bc6f4b58afe79bd4e42125cf88
maZero1 <tkmarlon594@gmail.com>
2025-11-10 20:57:21 -0300

    conexão com html e back-end

---
+++ src/main/java/br/com/guiarq/Controller/CustomErrorController.java
@@ -0,0 +1,15 @@
+package br.com.guiarq.Controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        // aqui você pode tratar o tipo de erro se quiser (404, 500, etc)
+        return "forward:/error/404.html";
+    }
+}
