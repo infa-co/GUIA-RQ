61735fa792c7d74651117a5046f4cdeb12b399ad
maZero1 <tkmarlon594@gmail.com>
2025-11-11 07:35:13 -0300

    classes de teste adicionadas

--- src/test/java/GuiaRQApplicationTests.java
+++ src/test/java/GuiaRQApplicationTests.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = GuiaRQApplication.class)
+@SpringBootTest(classes = GuiaRQApplicationTests.class)
 @ActiveProfiles("test")
 class GuiaRQApplicationTests {
 
