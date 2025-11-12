276c9288068008c2e802397cfe619269449d3798
maZero1 <tkmarlon594@gmail.com>
2025-11-11 22:09:14 -0300

    classes de teste adicionadas

--- src/main/java/br/com/guiarq/Model/Service/StripeService.java
+++ src/main/java/br/com/guiarq/Model/Service/StripeService.java
@@ -1,40 +1,47 @@
-/*package br.com.guiarq.Model.Service;
+package br.com.guiarq.Model.Service;
 
 import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import jakarta.annotation.PostConstruct;
 
-public class StripeService {
-    public StripeService() {
-        Stripe.apiKey = "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";
+@Service
+public class    StripeService {
+
+    @Value("${stripe.api.key}")
+    private String stripeApiKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = stripeApiKey;
     }
 
-    public String criarSessaoCheckout(String nomeTicket, double preco, String successUrl, String cancelUrl) throws Exception {
-        SessionCreateParams params =
-                SessionCreateParams.builder()
-                        .setMode(SessionCreateParams.Mode.PAYMENT)
-                        .setSuccessUrl(successUrl)
-                        .setCancelUrl(cancelUrl)
-                        .addLineItem(
-                                SessionCreateParams.LineItem.builder()
-                                        .setQuantity(1L)
-                                        .setPriceData(
-                                                SessionCreateParams.LineItem.PriceData.builder()
-                                                        .setCurrency("brl")
-                                                        .setUnitAmount((long) (preco * 100))
-                                                        .setProductData(
-                                                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
-                                                                        .setName(nomeTicket)
-                                                                        .build()
-                                                        )
-                                                        .build()
-                                        )
-                                        .build()
-                        )
-                        .build();
+    public Session createCheckoutSession(String successUrl, String cancelUrl, Long amount) throws StripeException {
+        SessionCreateParams params = SessionCreateParams.builder()
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl(successUrl)
+                .setCancelUrl(cancelUrl)
+                .addLineItem(
+                        SessionCreateParams.LineItem.builder()
+                                .setQuantity(1L)
+                                .setPriceData(
+                                        SessionCreateParams.LineItem.PriceData.builder()
+                                                .setCurrency("brl")
+                                                .setUnitAmount(amount)
+                                                .setProductData(
+                                                        SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                                .setName("Ticket Guia RQ")
+                                                                .build()
+                                                )
+                                                .build()
+                                )
+                                .build()
+                )
+                .build();
 
-        Session session = Session.create(params);
-        return session.getUrl(); // URL do checkout
+        return Session.create(params);
     }
 }
-*/
