b0da69ed84a284b8734326040a9fba613b13b83d
maZero1 <tkmarlon594@gmail.com>
2025-11-24 09:44:57 -0300

    fix: application-prod.properties

--- src/main/java/br/com/guiarq/Model/Repository/UsuarioRepository.java
+++ src/main/java/br/com/guiarq/Model/Repository/UsuarioRepository.java
@@ -4,6 +4,8 @@ import br.com.guiarq.Model.Entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
