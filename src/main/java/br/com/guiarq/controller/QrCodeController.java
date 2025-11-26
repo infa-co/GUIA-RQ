6ec2ae5e8e515647d62530862cde8c198052f805
macbookairm1 <juniorheinz72@gmail.com>
2025-11-25 22:17:42 -0300

    ajustes qr code

--- src/main/java/br/com/guiarq/controller/QrCodeController.java
+++ src/main/java/br/com/guiarq/controller/QrCodeController.java
@@ -37,7 +37,7 @@ public class QrCodeController {
         if (ticket == null)
             return ResponseEntity.notFound().build();
 
-        String conteudo = "https://guiaranchoqueimado.com.br/validar-ticket.html?id=" + ticket.getIdPublico();
+        String conteudo = "https://guiaranchoqueimado.com.br/docs/pages/validar-ticket.html?id=" + ticket.getIdPublico(); + ticket.getIdPublico();
 
         byte[] qr = qrCodeService.generateQrCodeBytes(conteudo, 400, 400);
 
