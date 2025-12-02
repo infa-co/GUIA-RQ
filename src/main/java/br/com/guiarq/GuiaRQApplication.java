ed645b3a4fa2b528bffa00ef2f0a6199c4703456
maZero1 <tkmarlon594@gmail.com>
2025-12-02 09:08:57 -0300

    fix: deploy do render

--- src/main/java/br/com/guiarq/GuiaRQApplication.java
+++ src/main/java/br/com/guiarq/GuiaRQApplication.java
@@ -10,13 +10,4 @@ public class GuiaRQApplication {
     public static void main(String[] args) {
         SpringApplication.run(GuiaRQApplication.class, args);
     }
-    @PostConstruct
-    public void debugEnv() {
-        System.out.println("====== VARIÁVEIS CAPTURADAS PELO SPRING ======");
-        System.out.println("JDBC_DATABASE_URL = " + System.getenv("JDBC_DATABASE_URL"));
-        System.out.println("DATABASE_URL = " + System.getenv("DATABASE_URL"));
-        System.out.println("SPRING_PROFILES_ACTIVE = " + System.getenv("SPRING_PROFILES_ACTIVE"));
-        System.out.println("===============================================");
-    }
-
 }
